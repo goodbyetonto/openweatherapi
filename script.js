16fdd44b03cd74b2f24f61b8abb0b224e601d9e2
Garrett <goodbyetonto@gmail.com>
2020-05-06 23:17:28 -0700

    working on the 5 day forecast, have unix conversion to mm/dd/yyyy

--- script.js
+++ script.js
@@ -1,46 +1,32 @@
 $(document).ready(function () {
-    
+    const daily = $(".daily"); 
+    daily.hide(); 
     // Define counter for 0
     let counter = 0;
     var zip = $("#zipField");
     var cityBtn = $(".cityBtn");
     var zipBtn = $(".zipBtn");
     var key = "54763b00d037c47de6a2388f5546c0da";
-    const queryCityExt = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=" + key;
     const iconURL = "http://openweathermap.org/img/wn/10d@2x.png"
     var queryZip = "api.openweathermap.org/data/2.5/forecast?id=" + zip + "&appid=" + key;
 
+    
     function addCity(city, counter) {
-
-
         const cityList = $(".city");
-
-
         newBtn = $("<button></button>");
-
-
         newBtn.attr({ type: "button", class: "list-group-item list-group-item-action", id: `city${counter}` });
-
-
         newBtn.html(city);
-
-
         cityList.append(newBtn);
-    }
+    };
 
-    function currentCond(resp) {
-
-
-
-    }
 
     function currentWeather(resp, city, respuv) {
-        let dateTime = moment().format('MMMM Do YYYY, h:mm:ss a');
-        let tempKel = resp.main.temp;
-        let tempFar = Math.round(tempKel * 9 / 5 - 459.67);
-        let uvI = respuv.value; 
-        let humid = resp.main.humidity; 
-        let windy = resp.wind.speed; 
+        const dateTime = moment().format('MMMM Do YYYY, h:mm:ss a');
+        const tempKel = resp.main.temp;
+        const tempFar = Math.round(tempKel * 9 / 5 - 459.67);
+        const uvI = respuv.value; 
+        const humid = resp.main.humidity; 
+        const windy = resp.wind.speed; 
         const cardBody = $(".card-body"); 
         const location = $("#city"); 
         const temp = $("#temp"); 
@@ -52,11 +38,26 @@ $(document).ready(function () {
         humidity.html("Humidity: " + humid + "%"); 
         wind.html("Wind Speed: " + windy + "mph"); 
         uv.html("UV Index: " + uvI); 
-    }
+    };
+
+    function qeuryExt(respext) {
+        debugger
+        const cardBody = $(".card-body"); 
+        let counter = 1; 
+        for (i = 0; i < 40; i += 8) {
+            const unixTime = respext.list[i].dt; 
+            const convTime = moment.unix(unixTime).format("MM/DD/YYYY");
+            const day = $(`.${counter}`); 
+            day.append($("<p></p>")); 
+            day.html(convTime); 
+            counter++; 
+        };
+        daily.show(); 
+    }; 
 
     function queryUvI(resp, city) {
-        let lat = resp.coord.lat; 
-        let lon = resp.coord.lon; 
+        const lat = resp.coord.lat; 
+        const lon = resp.coord.lon; 
         const uvIQuery = "https://api.openweathermap.org/data/2.5/uvi?appid=" + key + "&lat=" + lat + "&lon=" + lon;
         console.log(uvIQuery); 
         $.ajax({
@@ -65,19 +66,26 @@ $(document).ready(function () {
         }).then(function (respuv) {
             currentWeather(resp, city, respuv); 
         });
-    }
+    };
 
     // 'On-Click' for handling the Current Weather Query
     cityBtn.click(function () {
         counter++;
         var city = $("#cityField").val();
         const queryCityCurrent = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + key;
+        const queryCityExt = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=" + key;
         addCity(city, counter);
         $.ajax({
             url: queryCityCurrent,
             method: "GET"
         }).then(function (resp) {
             queryUvI(resp, city); 
+        });
+        $.ajax({
+            url: queryCityExt,
+            method: "GET"
+        }).then(function (respext) {
+            qeuryExt(respext); 
         });
     }); 
 });
