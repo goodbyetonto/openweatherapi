597590c46e5b3b02ab0a636260de32ad5a925ce9
Garrett <goodbyetonto@gmail.com>
2020-05-07 18:28:55 -0700

    I have the 5  day forecast working now

--- script.js
+++ script.js
@@ -3,12 +3,12 @@ $(document).ready(function () {
     daily.hide(); 
     // Define counter for 0
     let counter = 0;
-    var zip = $("#zipField");
+    //var zip = $("#zipField");
     var cityBtn = $(".cityBtn");
-    var zipBtn = $(".zipBtn");
+    //var zipBtn = $(".zipBtn");
     var key = "54763b00d037c47de6a2388f5546c0da";
-    const iconURL = "http://openweathermap.org/img/wn/10d@2x.png"
-    var queryZip = "api.openweathermap.org/data/2.5/forecast?id=" + zip + "&appid=" + key;
+    //const iconURL = "http://openweathermap.org/img/wn/10d@2x.png"
+    //var queryZip = "api.openweathermap.org/data/2.5/forecast?id=" + zip + "&appid=" + key;
 
     
     function addCity(city, counter) {
@@ -27,37 +27,46 @@ $(document).ready(function () {
         const uvI = respuv.value; 
         const humid = resp.main.humidity; 
         const windy = resp.wind.speed; 
-        const cardBody = $(".card-body"); 
+        //const cardBody = $(".card-body"); 
         const location = $("#city"); 
         const temp = $("#temp"); 
         const humidity = $("#humidity");
         const wind = $("#wind");
         const uv = $("#uv");
-        location.html(city + "( " + dateTime + " )"); 
-        temp.html("Tempearature: " + tempFar + "°F"); 
+        location.text(city + "( " + dateTime + " )"); 
+        temp.text("Tempearature: " + tempFar + "°F"); 
         humidity.html("Humidity: " + humid + "%"); 
-        wind.html("Wind Speed: " + windy + "mph"); 
-        uv.html("UV Index: " + uvI); 
+        wind.text("Wind Speed: " + windy + "mph"); 
+        uv.text("UV Index: " + uvI); 
     };
 
+
     function qeuryExt(respext) {
-        const cardBody = $(".card-body"); 
         let counter = 1; 
         for (i = 0; i < 40; i += 8) {
             const unixTime = respext.list[i].dt; 
             const convTime = moment.unix(unixTime).format("MM/DD/YYYY");
-            const tempKel = respext.list[i].main.temp; 
-            const tempFar = Math.round(tempKel * 9/5 - 459.67);
+            let tempLoSum = 0; 
+            let tempHiSum = 0; 
+            for (j = 0; j < 8; j++) {
+                const tempKelMin = respext.list[i].main.temp_min; 
+                const tempKelMax = respext.list[i].main.temp_max;
+                tempLoSum += tempKelMin; 
+                tempHiSum += tempKelMax;
+            }
+            const avgMin = tempLoSum/8; 
+            const avgMax = tempHiSum/8;
+            const tempFarMin = Math.round(avgMin * 9/5 - 459.67);
+            const tempFarMax = Math.round(avgMax * 9/5 - 459.67);
             const humid = respext.list[i].main.humidity; 
             const day = $(`.${counter}`); 
-            const date = $("<p></p>").html(convTime); 
-            const temp = $("<p></p>").html(tempFar); 
-            const humidity = $("<p></p>").html(humid); 
-            day.append(date, temp, humidity);
-
-            
+            const date = $("<p></p>").text(convTime); 
+            const tempLo = $("<p></p>").text("Low Temp: " + tempFarMin + "°F"); 
+            const tempHi = $("<p></p>").text("Hight Temp: " + tempFarMax + "°F"); 
+            const humidity = $("<p></p>").text("Humidity: " + humid + "%"); 
+            day.append(date, tempLo, tempHi, humidity);
             counter++; 
-        };
+        }
         daily.show(); 
     }; 
 
